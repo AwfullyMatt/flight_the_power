206983658ff8c1795d2bd42a022f713ae6855d47
AwfullyMatt <the.xeronite@gmail.com>
2025-02-01 20:10:53 -0700

    +credits/licenses

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
 use bevy::app::{App, AppExit};
 use flightthepower::GamePlugin;
 
